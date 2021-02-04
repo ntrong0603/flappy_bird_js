f81bee318087feaf7bd25790efa658b0938c53a6
Lê Trọng <le.trong.rcvn2012@gmail.com>
2021-02-04 11:46:51 +0700

    Add function create pipe, backfound, footer background

--- main.js
+++ main.js
@@ -1,61 +1,115 @@
 const cvs = document.getElementById("board");
 const ctx = cvs.getContext("2d");
 // khai báo thành phần trong game
-let bird = new Image();
+var bird = new Image();
 bird.src = "images/bird.png";
 
-let bg = new Image();
+var bg = new Image();
 bg.src = "images/bg.png";
 
-let fg = new Image();
+var fg = new Image();
 fg.src = "images/fg.png";
 
-let pipeNorth = new Image();
+var pipeNorth = new Image();
 pipeNorth.src = "images/pipeNorth.png";
 
-let pipeSouth = new Image();
+var pipeSouth = new Image();
 pipeSouth.src = "images/pipeSouth.png";
 // Âm thanh
-let fly = new Audio();
+var fly = new Audio();
 fly.src = "sounds/fly.mp3";
 
-let scor = new Audio();
+var scor = new Audio();
 scor.src = "sounds/score.mp3";
 // =============
+//kích thước khung hình
+cvs.width = window.innerWidth;
+cvs.height = 512;
 // khai báo các giá trị ban đầu
 // Khoảng cách giữa 2 ống
-let gap = 125;
+var gap = 125;
 // Tọa độ ống nước dưới
-let constant;
+var constant;
 
 // Tọa độ của chim
-let bX = 10;
-let bY = 150;
+var bX = 10;
+var bY = 150;
 
 // mức độ rơi của chim (1,5 pixel)
-let gravity = 1.5;
+var gravity = 1.5;
+var jump = 30;
 
-let score = 0;
+var score = 0;
 
 // Mảng ống nước
 var pipe = [];
+var movePipeX = 0;
 
-// ống nước ban đầu
-pipe[0] = {
-    x: cvs.width,
-    y: 0
-};
-
-let endGame = false;
+var playGame = false;
+var endGame = false;
 
 document.addEventListener("keydown", moveUp);
 
+function main() {
+    createdPipeFirst();
+}
+main();
+function createdPipeFirst() {
+    var pipeX = gap;
+    var length = Math.round(cvs.width / gap) + 1;
+    if (length == 'Infinity') {
+        length = 3;
+    }
+    for (var i = 0; i < length; i++) {
+        pipe.push({
+            x: pipeX,
+            y: Math.floor(Math.random() * pipeNorth.height) - pipeNorth.height
+        });
+        pipeX += gap;
+    }
+}
+function createBackgournd() {
+    var widthBg = bg.width;
+    var length = Math.round(cvs.width / widthBg);
+    if (length == 'Infinity') {
+        length = 3;
+    }
+    var pointX = 0;
+    for (var i = 0; i < length; i++) {
+        ctx.drawImage(bg, pointX, 0);
+        pointX += widthBg;
+    }
+}
+function createFooterBackgournd() {
+    var widthBg = fg.width;
+    var length = Math.round(cvs.width / widthBg) + 1;
+    if (length == 'Infinity') {
+        length = 3;
+    }
+    var pointX = 0;
+    for (var i = 0; i < length; i++) {
+        ctx.drawImage(fg, pointX, cvs.height - fg.height);
+        pointX += widthBg;
+    }
+    // Footerback ground
+}
 function play() {
-    //
+    bX = 10;
+    bY = 150;
+    // Mảng ống nước
+    pipe = [];
+
+    // ống nước ban đầu
+    pipe[0] = {
+        x: cvs.width,
+        y: 0
+    };
+    endGame = false;
+    draw();
 }
 
 function moveUp() {
-    bY -= 25;
+    bY -= jump;
     if (bY <= 0) {
         bY = 0;
     }
@@ -68,9 +122,16 @@ function draw() {
         return;
     }
     ctx.clearRect(0, 0, cvs.width, cvs.height);
-    ctx.drawImage(bg, 0, 0);
-
-
+    createBackgournd();
+    movePipeX++;
+    if (movePipeX == gap) {
+        pipe.push({
+            x: pipe[pipe.length - 1].x + gap,
+            y: Math.floor(Math.random() * pipeNorth.height) - pipeNorth.height
+        });
+        console.log = "11";
+        movePipeX = 0;
+    }
     for (var i = 0; i < pipe.length; i++) {
 
         constant = pipeNorth.height + gap;
@@ -79,12 +140,7 @@ function draw() {
 
         pipe[i].x--;
 
-        if (pipe[i].x == 125) {
-            pipe.push({
-                x: cvs.width,
-                y: Math.floor(Math.random() * pipeNorth.height) - pipeNorth.height
-            });
-        }
+        
 
         if (pipe[i].x < -(pipeNorth.width + 10)) {
             pipe.shift();
@@ -107,9 +163,7 @@ function draw() {
 
 
     }
-
-    ctx.drawImage(fg, 0, cvs.height - fg.height);
-
+    createFooterBackgournd();
     ctx.drawImage(bird, bX, bY);
 
     bY += gravity;
